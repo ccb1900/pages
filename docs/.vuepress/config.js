cb128301f37321fa58d1cdd21f82d6078a0e2c7a
ccb <ccb412727@qq.com>
2020-06-05 10:02:18 +0800

    AMQP 初探

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  title: '献给新一代的文档',
+  // theme: '@vuepress/blog',
+  base: '/pages/',
+  description: 'java,mysql,php,redis,消息队列',
+  themeConfig: {
+    displayAllHeaders: true,
+    lastUpdated: '最近更新时间',
+    smoothScroll: true,
+    // sidebar: 'auto',
+    nav: [
+      { text: '主页', link: '/' },
+      { text: '消息队列', link: '/mq/' },
+      { text: 'Java', link: '/java/' },
+      { text: 'PHP', link: '/php/' },
+      { text: 'Mysql', link: '/mysql/' },
+      { text: 'Redis', link: '/redis/' },
+      { text: '其他', link: '/other/' },
+      // { text: 'External', link: 'https://google.com' },
+    ],
+    sidebar: {
+      '/mq/': [
+        '',     /* /foo/ */
+        'AMQP',   /* /foo/two.html */
+        '发布者',   /* /foo/two.html */
+        '消费者',   /* /foo/two.html */
+      ],
+
+      '/java/': [
+        '',      /* /bar/ */
+      ],
+
+      // fallback
+      '/': [
+        '',        /* / */
+        '/java/', /* /contact.html */
+        '/mq/'    /* /about.html */
+      ]
+    }
+  }
+}
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,16 @@
-# Hello VuePress!
+---
+home: true
+heroImage: /logo3.png
+heroText: 献给新一代的文档
+tagline: 那么，奔涌吧，后浪！
+actionText: 像我一样 →
+actionLink: /guide/
+features:
+- title: 满怀羡慕
+  details: 人类积攒了几千年的财富，所有的知识、见识、智慧和艺术，像是专门为你们准备的礼物。
+- title: 满怀敬意
+  details: 向你们的专业态度致敬，你们正在把传统的变成现代的，把经典的变成流行的，把学术的变成大众的，把民族的变成世界的。
+- title: 满怀感激
+  details: 因为你们这世上的小说、音乐、电影所表现的青春就不再是忧伤、迷茫，而是善良、勇敢、无私、无所畏惧，是心里有火，眼里有光，不用活着我们想象中的样子。
+footer: MIT Licensed | Copyright © {{ (new Date()).getYear() }} -present 后浪
+---
