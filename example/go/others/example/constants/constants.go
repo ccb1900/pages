da185926d3c77d1d02023b5f887d4376dc301c49
ccb1900 <guojianhang1992@outlook.com>
2020-08-07 11:49:04 +0800

    example

---
+++ example/go/others/example/constants/constants.go
@@ -0,0 +1,16 @@
+package constants
+
+import "fmt"
+
+func Constants() {
+	const n = 10000
+
+	fmt.Println(n)
+
+	fmt.Println(n)
+	fmt.Println(n)
+	fmt.Println(n)
+	fmt.Println(n)
+
+	fmt.Println(n)
+}
---
+++ example/go/others/example/variables/variables.go
@@ -0,0 +1,17 @@
+package variables
+
+import "fmt"
+
+func Variables() {
+	var a = "initial"
+	fmt.Println(a)
+
+	var b int = 1
+	fmt.Println(b)
+	var c = true
+	fmt.Println(c)
+	var d int
+	fmt.Println(d)
+	var e = "apple"
+	fmt.Println(e)
+}
