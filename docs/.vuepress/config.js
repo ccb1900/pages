43517cb85255bff9769902df01492edda35cde2b
ccb1900 <guojianhang1992@outlook.com>
2021-04-21 20:22:04 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -29,7 +29,7 @@ module.exports = {
       },
     ],
   ],
-  base: "/pages/",
+  base: "/",
   logo: "/logo.png",
   description: "java,mysql,php,redis,消息队列,laravel,spring",
   head: [["link", { rel: "ico", href: `/logo.png` }]],
